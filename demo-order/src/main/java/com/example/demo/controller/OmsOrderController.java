3df0a55c160796fe4e0c3dfaa58ee9f6a28c8a7e
袁健伟 <2754742370@qq.com>
2023-09-07 11:59:52 +0800

    add

--- demo-order/src/main/java/com/example/demo/controller/OmsOrderController.java
+++ demo-order/src/main/java/com/example/demo/controller/OmsOrderController.java
@@ -29,11 +29,23 @@ public class OmsOrderController {
 
     @ApiOperation(value = "新增订单信息")
     @PostMapping(value = "/add", produces = "application/json;charset=UTF-8")
-    public CommonResult add(@RequestBody OmsOrder omsOrder){
+    public CommonResult add(@RequestBody OmsOrder omsOrder) {
         int count = omsOrderService.insert(omsOrder);
         if (count > 0) {
             return CommonResult.success(count);
         }
         return CommonResult.failed();
+    }
+
+    @ApiOperation(value = "关联流控模式-查询")
+    @GetMapping(value = "/query")
+    public CommonResult query() {
+        return CommonResult.success("查询成功");
+    }
+
+    @ApiOperation(value = "关联流控模式-更新")
+    @PostMapping(value = "/update")
+    public CommonResult update() {
+        return CommonResult.success("更新成功");
     }
 }
